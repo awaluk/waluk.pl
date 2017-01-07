ccbdd462dc46a8878ff5464dbaa0b010469ff486
Arkadiusz Waluk <arkadiusz@waluk.pl>
2017-01-07 19:05:28 +0100

    Repair margin after scrolling at small screens

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
 $('.menu__link').click(function (){
-    $('body').animate({scrollTop: $(this.hash).offset().top-40}, 800);
+    var margin = ($(document).width() <= 500 ? 90 : 40);
+    $('body').animate({scrollTop: $(this.hash).offset().top-margin}, 800);
 });
 $('#to-top, #logo').click(function () {
     $('body').animate({scrollTop: $(this.hash).offset().top}, 800);

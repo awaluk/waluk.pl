8afc6978efc856f015fd54901bef97b7e8e5bfac
Arkadiusz Waluk <arkadiusz@waluk.pl>
2021-06-21 17:07:39 +0200

    Fix lists line-height, add HTML and JS to code blocks

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,15 @@
 import hljs from 'highlight.js/lib/core';
 import php from 'highlight.js/lib/languages/php';
+import xml from 'highlight.js/lib/languages/xml';
+import javascript from 'highlight.js/lib/languages/javascript';
 import 'highlight.js/styles/darcula.css';
 
 import 'normalize.css/normalize.css';
 import './styles/app.scss';
 
 hljs.registerLanguage('php', php);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('javascript', javascript);
 hljs.highlightAll();
 
 const classOpen = 'header__navigation--open';
